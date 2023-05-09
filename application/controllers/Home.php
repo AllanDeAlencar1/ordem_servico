88dcd7ce4b5dd7cbe00b083bab54870cdd48b456
Allan de Alencar Ramos <allandealencar1@gmail.com>
2023-05-08 22:12:34 -0300

    Separando os componentes do template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,7 +11,9 @@ class Home extends CI_Controller {
 
     public function index()
     {
+        $this->load->view('common/header');
         $this->load->view('home/index');
+        $this->load->view('common/header');
     }
 
 
